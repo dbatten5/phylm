57bb816dd019ea4af9ae8d742ad98cfbbadb90a7
Dom Batten <dominic.batten@googlemail.com>
2021-03-11 10:32:53 +0000

    Add imdb source

--- movie.py
+++ movie.py
@@ -1,7 +1,11 @@
 from sources.mtc import Mtc
+from sources.rt import Rt
+from sources.imdb import Imdb
 
 class Movie:
     def __init__(self, title, year):
         self.title = title
         self.year = year
         self._mt_data = Mtc(title)
+        self._rt_data = Rt(title)
+        self._imdb_data = Imdb(title)
---
+++ sources/imdb.py
@@ -0,0 +1,55 @@
+import imdb
+
+class Imdb:
+    ia = imdb.IMDb()
+
+    def __init__(self, raw_title):
+        self.raw_title = raw_title
+        self.low_confidence = False
+        self._imdb_data = self._get_imdb_data()
+
+    def _get_imdb_data(self):
+        results = Imdb.ia.search_movie(self.raw_title)
+        target = None
+        for result in results:
+            if result['title'].lower() == self.raw_title.lower():
+                target = result
+                break
+        if not target:
+            result = results[0]
+            self.low_confidence = True
+        Imdb.ia.update(target, info=['main'])
+        return target
+
+    def title(self):
+        return self._imdb_data['title']
+
+    def genres(self):
+        return ', '.join(self._imdb_data['genres'])
+
+    def cast(self, limit=5):
+        cast_members = [person['name'] for person in self._imdb_data['cast'][:limit]]
+        return ', '.join(cast_members)
+
+    def runtime(self):
+        return self._imdb_data['runtimes'][0]
+
+    def directors(self):
+        try:
+            directors = [person['name'] for person in self._imdb_data['directors']]
+        except Exception:
+            return None
+        return ', '.join(directors)
+
+    def score(self):
+        return self._imdb_data['rating']
+
+    def plot(self):
+        data = self._imdb_data
+        if 'plot' not in data.current_info:
+            Imdb.ia.update(data, info=['plot'])
+        try:
+            plot = data['plot'][0].split('::')[0]
+        except Exception:
+            plot = ''
+        return plot
