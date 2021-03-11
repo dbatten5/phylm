f082df7c1dfee8a9a3589b0c4b1e19810584f050
Dom Batten <dominic.batten@googlemail.com>
2021-03-11 16:29:20 +0000

    Add docstrings to utils/web.py

--- phylm/utils/web.py
+++ phylm/utils/web.py
@@ -1,13 +1,17 @@
+"""Module to contain some web helper functions"""
+
+from urllib.parse import quote_plus
 import requests
 from bs4 import BeautifulSoup
-from urllib.parse import quote_plus
 
 
 def soupify(url):
+    """Get a webpage and return the BeautifulSoup representation"""
     user_agent = {'User-agent': 'Mozilla/5.0'}
     search = requests.get(url, headers=user_agent).text
     return BeautifulSoup(search, 'html.parser')
 
 
 def url_encode(string):
+    """URL encode a string to be used in search queries"""
     return quote_plus(string)
