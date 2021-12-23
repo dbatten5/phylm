97bba1deb43a8b48de5c33dd7a6aa4e62dd6aa1c
Dom Batten <dominic.batten@googlemail.com>
2021-12-23 17:58:38 +0000

    add `async_soupify` function

--- src/phylm/utils/web.py
+++ src/phylm/utils/web.py
@@ -1,8 +1,13 @@
 """Module to contain some web helper functions."""
+from typing import Optional
 from urllib.parse import quote_plus
 
 import requests
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
+
+
+DEFAULT_HEADERS = {"User-agent": "Mozilla/5.0"}
 
 
 def soupify(url: str) -> BeautifulSoup:
@@ -14,9 +19,36 @@ def soupify(url: str) -> BeautifulSoup:
     Returns:
         a `BeautifulSoup` representation of the given url
     """
-    user_agent = {"User-agent": "Mozilla/5.0"}
-    search = requests.get(url, headers=user_agent).text
+    search = requests.get(url, headers=DEFAULT_HEADERS).text
     return BeautifulSoup(search, "html.parser")
+
+
+async def async_soupify(
+    url: str, session: Optional[ClientSession] = None
+) -> BeautifulSoup:
+    """Asynchronously get a webpage and return the BeautifulSoup representation.
+
+    Args:
+        url: the url for scraping
+        session: an optional instance of `aiohttp.ClientSession` in which to run the
+            request. If a session is passed here then it will remain open after this
+            function returns.
+
+    Returns:
+        a `BeautifulSoup` representation of the given url
+    """
+    keep_session = False
+    if session:
+        keep_session = True
+
+    session = session or ClientSession()
+    async with session.get(url, headers=DEFAULT_HEADERS) as resp:
+        html = await resp.text()
+
+    if not keep_session:
+        await session.close()
+
+    return BeautifulSoup(html, "html.parser")
 
 
 def url_encode(string: str) -> str:
